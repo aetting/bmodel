21b821dfd72bbe496070fb1bedf546115f97c242
aetting <allyson.k.ettinger@gmail.com>
2017-08-20 21:20:10 -0400

    prep for 3b

--- brouwer_config.py
+++ brouwer_config.py
@@ -3,7 +3,7 @@ modelID = '3b'
 context_size = 200
 retrieval_size = 80
 
-trainingsuf = 'br-origfulldutch'
+trainingsuf = 'br-sterdutch8k'
 
 traincode='p2'
 
@@ -12,10 +12,10 @@ itperup = 100 #100 in Brouwer
 
 reducelr = True
 
-embdic='brouwerCOALS-100.txt'
+embdic='embs/brouwerCOALS-100.txt'
 binary = True
 
 # dict='brouwerGloVe-100.txt'
 # trainingsuf = 'br-origfulleng'
 
-notes='Next?'
+notes='full model only training with stereotypical only'
