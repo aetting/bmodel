4786bc451777a098b673639903ca9d78f720458f
aetting <aetting@umd.edu>
2017-08-21 10:09:11 -0400

    add 3c

--- brouwer_config.py
+++ brouwer_config.py
@@ -1,14 +1,14 @@
-modelID = 'test'
+modelID = '3c'
 
 context_size = 200
 retrieval_size = 80
 
-trainingsuf = 'br-origfulldutch'
-# trainingsuf = 'br-sterdutch8k'
+#trainingsuf = 'br-origfulldutch'
+trainingsuf = 'br-nonsterdutch8k'
 
 traincode='p2'
 
-maxup = 7000 #7000 in Brouwer
+maxup = 10000 #7000 in Brouwer
 itperup = 100 #100 in Brouwer
 
 reducelr = False
@@ -19,4 +19,4 @@ binary = True
 # dict='brouwerGloVe-100.txt'
 # trainingsuf = 'br-origfulleng'
 
-notes='full model only training with stereotypical only'
+notes='full model only training with NONstereotypical only'
