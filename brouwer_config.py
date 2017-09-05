aa0e9c7fc5394b21ea7f0be493c1779434790ff9
aetting <aetting@umd.edu>
2017-09-04 20:39:31 -0400

    add 4e-g

--- brouwer_config.py
+++ brouwer_config.py
@@ -1,9 +1,9 @@
-modelID = '4e'
+modelID = '4g'
 
 context_size = 200
 retrieval_size = 80
 
-trainingsuf = 'br-orig-ster0.5-dutch'
+trainingsuf = 'br-orig-ster0.25-dutch'
 # trainingsuf = 'br-origfulldutch'
 # trainingsuf = 'br-holdoutB-dutch'
 #trainingsuf = 'br-nonsterdutch8k'
@@ -16,10 +16,10 @@ itperup = 100 #100 in Brouwer
 embdic='embs/brouwerCOALS-100.txt'
 binary = True
 
-reducelr=True
+reducelr=False
 tryloc=False
 
 # dict='brouwerGloVe-100.txt'
 # trainingsuf = 'br-origfulleng'
 
-notes='try .5 of ster data again -- this time with reducing lr'
+notes='try .25 of ster data -- now with constant lr'
